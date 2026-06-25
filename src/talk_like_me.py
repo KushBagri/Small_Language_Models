6b3dd74aa7d67511c7db85e810f7090c6c66a8fd
Kush Bagri <kush.bagri2002@gmail.com>
2026-06-25 19:01:40 +0530

    feat: added blocks of alternating attention & feedforward layers

--- src/talk_like_me.py
+++ src/talk_like_me.py
@@ -99,7 +99,20 @@ class MultiHeadAttention(nn.Module):
         
         return out
 
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation"""
 
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd//n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+
+    def forward(self, x):
+        x = self.sa(x)
+        x = self.ffwd(x)
+        return x
+    
 class GPTLanguageModel(nn.Module):
 
     def __init__(self):
@@ -107,8 +120,11 @@ class GPTLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_heads = MultiHeadAttention(4, n_embd//4)
-        self.ffwd = FeedForward(n_embd)
+        self.blocks = nn.Sequential(
+            Block(n_embd, n_head = 4),
+            Block(n_embd, n_head = 4),
+            Block(n_embd, n_head = 4)
+        )
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
 
@@ -119,8 +135,7 @@ class GPTLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
         x = tok_emb + pos_emb # (B,T,C)
-        x = self.sa_heads(x) # applying one head of self attention
-        x = self.ffwd(x)
+        x = self.blocks(x)
         logits = self.lm_head(x) # (B,T,vocab_size)
 
         if targets is None:
