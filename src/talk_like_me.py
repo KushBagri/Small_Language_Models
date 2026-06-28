8671a221a5c0c6e2e66fccd4a188259f59d9b035
Kush Bagri <kush.bagri2002@gmail.com>
2026-06-28 17:34:47 +0530

    feat: adding a linear projection after the multi-head attention so that the information extracted by the individual heads can tbe shared and combined

--- src/talk_like_me.py
+++ src/talk_like_me.py
@@ -93,10 +93,11 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
         
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
-        
+        out = self.proj(out)    
         return out
 
 class Block(nn.Module):
